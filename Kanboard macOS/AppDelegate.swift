9119fb839c959a44f2cde21a62b4dea578a5cc75
Dmytro Vorobiov <d@dvor.me>
2018-05-09 12:18:10 +0200

    Initial commit

---
+++ Kanboard macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kanboard macOS
+//
+//  Created by Dmytro Vorobiov on 09/05/2018.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+[Kanboard](https://kanboard.org/) app for macOS and iOS.
+
+## WIP
+
+In development, nothing to see here yet.
+
+## Contact
+
+[d@dvor.me](mailto:d@dvor.me?subject=TextWiki)
+
+## License
+
+TextWiki is available under the MIT license. See the [LICENSE](LICENSE) file for more info.
+
