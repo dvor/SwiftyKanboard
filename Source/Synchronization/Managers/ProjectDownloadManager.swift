e00401b356778b78992f95522639a1f21e7ff260
Dmytro Vorobiov <d@dvor.me>
2018-06-13 23:50:40 +0200

    Downloading columns and tasks

--- Source/Synchronization/Managers/ProjectDownloadManager.swift
+++ Source/Synchronization/Managers/ProjectDownloadManager.swift
@@ -33,22 +33,49 @@ extension ProjectDownloadManager: DownloadManager {
     }
 
     func synchronizeRequiredSettings(completion: @escaping (() -> Void), failure: @escaping ((NetworkServiceError) -> Void)) {
-        let request = GetProjectByIdRequest(projectId: projectId, completion: { project in
-            let realm = try! Realm.default()
-
-            let updater: DatabaseUpdater<RemoteProject, Project> = DatabaseUpdater(realm: realm)
-            _ = updater.updateDatabase(with: project)
-
-            completion()
-        },
-        failure: failure)
-
+        let request = syncProject()
         downloadQueue.add(downloadRequest: request, isConcurent: true)
     }
 }
 
 private extension ProjectDownloadManager {
     func doFullSync() {
+        downloadQueue.add(downloadRequest: syncProject(), isConcurent: true)
+        downloadQueue.add(downloadRequest: syncColumns(), isConcurent: true)
+        downloadQueue.add(downloadRequest: syncTasks(active: true), isConcurent: true)
+    }
 
+    func syncProject() -> GetProjectByIdRequest {
+        return GetProjectByIdRequest(projectId: projectId, completion: { project in
+            let realm = try! Realm.default()
+
+            let updater: DatabaseUpdater<RemoteProject, Project> = DatabaseUpdater(realm: realm)
+            _ = updater.updateDatabase(with: project)
+        },
+        failure: { _ in })
+    }
+
+    func syncColumns() -> GetColumnsRequest {
+        return GetColumnsRequest(projectId: projectId, completion: { columns in
+            let realm = try! Realm.default()
+
+            let updater: DatabaseUpdater<RemoteColumn, Column> = DatabaseUpdater(realm: realm)
+            columns.forEach {
+                _ = updater.updateDatabase(with: $0)
+            }
+        },
+        failure: { _ in })
+    }
+
+    func syncTasks(active: Bool) -> GetAllTasksRequest {
+        return GetAllTasksRequest(projectId: projectId, active: active, completion: { tasks in
+            let realm = try! Realm.default()
+
+            let updater: DatabaseUpdater<RemoteTask, Task> = DatabaseUpdater(realm: realm)
+            tasks.forEach {
+                _ = updater.updateDatabase(with: $0)
+            }
+        },
+        failure: { _ in })
     }
 }
